e75f72bc1aecd02c89a5cbd302e36b2c623c700d
nguyencuong16 <111492899+nguyencuong16@users.noreply.github.com>
2022-08-18 15:10:19 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thùy Pu💗';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100012083268298';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người yêu anh <3';
+const contentCard = 'Chúc Honey của anh một ngày sinh nhật vui vẻ tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau, anh yêu em ❤️';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thuypu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
